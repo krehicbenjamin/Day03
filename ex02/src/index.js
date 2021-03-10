21d74acfaeb7b3fd46223ae225e2638a30065424
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-10 16:50:10 +0100

    Adding the second task

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //Only change code below this line
+
+        // Five or more characters
+
+        /* Five
+            or
+           more
+        characters
+        */
+
+    //Only change code above this line
+}
+
+main();
+
+module.exports = main;
