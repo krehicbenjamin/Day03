972fde98bad757a918cd8a9bc44f2b9dc3968190
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-10 20:02:08 +0100

    Removed a space on var z

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -4,13 +4,13 @@ function main() {
         
         var x = 7;
         var y = 12;
-        var z = "My name is ";
+        var z = "My name is";
 
     //Only change code above this line
 
     x = x + 2;
     y = y + 4;
-    z = z + "John!";
+    z = z + " John!";
 
     return [x, y, z];
 }
