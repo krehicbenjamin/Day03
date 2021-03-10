bc4c4976fd48c364b3d455b4a056c7add9a0adc9
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-10 16:45:54 +0100

    fixing accidental delete on coments

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,7 +1,9 @@
 function main() {
+    //Only change code below this line
 
-    console.log('Hello World!');
+        console.log('Hello World!');
 
+    //Only change code above this line
 }
 
 main();
