e7822599c59de4c179273245142110d876547565
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-10 20:25:39 +0100

    Adding the 9th task

---
+++ ex09/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+
+        var sum = numOne - numTwo;
+        
+
+    //Only change code above this line
+
+    return difference;
+
+}
+
+console.log(main(numOne, numTwo));
+
+module.exports = main;
