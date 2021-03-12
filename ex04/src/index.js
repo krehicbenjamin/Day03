e379f5f5aa7ecb17b90027d6816fd114e247fe8f
KrehicBenjamin <krehicbenjamin@gmail.com>
2021-03-12 14:02:17 +0100

    Fixing code

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -2,8 +2,8 @@ function main() {
     //Only change code below this line
 
         
-        var x=10;
-        var y=x;
+        var x = 10;
+        var y = x;
 
     //Only change code above this line
     return [x, y];
